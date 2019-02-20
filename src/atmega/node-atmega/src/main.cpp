30bbed3996431b84250b03adc12c6847f5705eb9
F0xedb <tommeyers.tm127@gmail.com>
2019-02-20 16:55:47 +0100

    Added default files

---
+++ src/atmega/node-atmega/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+// TODO : start I²C communication with each sensor (master)
+// TODO : start I²C communication with RPi (slave)
+// TODO : Control lights
+// TODO : Control pumps
+
+void setup() {
+}
+
+void loop() {
+}
---
+++ src/node-master/launch.py
@@ -0,0 +1,5 @@
+# This applications handles all the threads
+# I prefer to split all the threads into different applications for easier control
+# This also avoids deadlock because all inter thread communication will happen over ports
+import subprocess
+
