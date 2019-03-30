a83799a1c25dafc5f9a19932d13768acab9aa7f3
Matthias Willemsen <me@mattwill.be>
2019-03-30 14:01:55 +0100

    Added routes

--- src/server/api/server.js
+++ src/server/api/server.js
@@ -10,12 +10,12 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }, (e
     console.log('Database connected');
 });
 
-const apiV1Nodes = require('./v1/api')(app, mongoose);
+const apiV1 = require('./v1/api')(app, mongoose);
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/v1/nodes', apiV1Nodes);
+app.use('/v1', apiV1);
 
 app.get('*', (req, res) => {
   res.sendStatus(403);
--- src/server/api/v1/api.js
+++ src/server/api/v1/api.js
@@ -1,12 +1,24 @@
-const router = require('express').Router();
+const router = require('express').Router({mergeParams: true});
 
 module.exports = function (app, db) {
 
-  router.get('/:id', (req, res) => {
-    res.sendStatus(201);
+  // Get all nodes
+  router.get('/nodes', (req, res) => {
+    res.send('nodes');
   });
 
-  router.get('/', (req, res) => {
+  // Get all sensors in a specific node
+  router.get('/nodes/:nodeid/sensors', (req, res) => {
+    res.send(req.params.nodeid + ' sensors');
+  });
+
+  // Get sensor data from a specific sensor in a specific node
+  router.get('/nodes/:nodeid/sensors/:sensorid/:amount?', (req, res) => {
+    if (!req.params.amount) req.params.amount = 1
+    res.send(req.params.nodeid + ' sensor ' + req.params.sensorid + ' amount ' + req.params.amount);
+  });
+
+  router.get('*', (req, res) => {
     res.sendStatus(404);
   });
 
