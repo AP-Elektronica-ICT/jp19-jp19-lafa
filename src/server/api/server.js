a95748c3997ad95d8e9227418e44d9fbd084c063
Matthias Willemsen <me@mattwill.be>
2019-04-01 11:44:43 +0200

    Commented out Populate Dev Routes

--- src/server/api/server.js
+++ src/server/api/server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/v1', require('./v1/api')(mongoose));
-app.use('/populate', require('./v1/populate')(mongoose));
+// For debug purposes only
+// app.use('/populate', require('./v1/populate')(mongoose));
 
 app.get('*', (req, res) => {
   res.sendStatus(403);
