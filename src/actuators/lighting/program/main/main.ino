40d905dee6b1dbd03c5ac62173b82712797e16f2
Ben Vinck <ben_vinck@yahoo.com>
2019-04-01 16:25:47 +0200

    Fixed issues #58 and #59
    The bug in question is fixed but there still exists an overflow issue with millis()

--- src/actuators/lighting/program/main/main.ino
+++ src/actuators/lighting/program/main/main.ino
@@ -1,33 +1,42 @@
 /*
 Light controller
-Recieve 3 chars via wire (currently on channel 0)
+Recieve 3 chars via wire (currently on channel 20)
 These 3 chars get stored in an array and converted to a int before analogWriting it to pin 1
 It has to recieve 3 chars otherwise it will be out of sync with the incomming data. (Eg: "236", "133", "053", "009")
+If there is a 3ms second delay between expected bytes it will dropp all previous recieved bytes
 */
 
 #include <TinyWire.h>
 #include <twi.h>
 
 char recievedWire[] = {0,0,0};
-int recievedValue;
+int recievedBytes = 0;
+int recievedValue = 0;
+
+long recieveTime = 0;
+long currentTime = millis();
 
 void setup() {
   pinMode(1, OUTPUT);
-  TinyWire.begin(0);
-  TinyWire.onReceive(recieveEvent);
+  TinyWire.begin(20);
+
 }
 
 void loop() {
-  delay(1);
-}
-
-void recieveEvent(){
-  if(TinyWire.available() >= 3){
-    for(int i = 0; i < 3; i++){
-      recievedWire[i] = TinyWire.read();
-    }
+  if(TinyWire.available() >= 1){
+    recievedWire[recievedBytes] = TinyWire.read();
+    recievedBytes++;
+    recieveTime = millis();
+  }
+  
+  currentTime = millis();
+  if(currentTime >= recieveTime + 3){
+    recievedBytes = 0;
+  }
+  
+  if(recievedBytes == 3){
     recievedValue = atoi(recievedWire);
     analogWrite(1, recievedValue);
+    recievedBytes = 0;
   }
 }
-
