064a651c5200e5ae098e4d146a63f49adb8c5eb2
Matthias Willemsen <me@mattwill.be>
2019-04-27 20:02:13 +0200

    Added all sensor & actuator types

--- src/server/api/v1/api.js
+++ src/server/api/v1/api.js
@@ -28,8 +28,11 @@ module.exports = function (db) {
         qos: 1
       });
     }
-  }
+  };
 
+  /**
+   * Create MQTT Server
+   */
   const server = new mosca.Server({
     port: 1883,
     backend: {
@@ -40,63 +43,136 @@ module.exports = function (db) {
     }
   });
 
+  /**
+   * MQTT Server on Ready
+   */
   server.on('ready', () => {
     console.log('MQTT Server Running');
     server.authenticate = authenticate;
   });
 
-  // TODO: Automatic sensor & actuator creation
+  /**
+   * MQTT Server on Subscribe (Client)
+   */
   server.on('subscribed', (topic, client) => {
-    if (topic == 'id') {
-      const lightintAct = new Actuator({
-        label: 'Light Strip',
-        type: 'lightint',
-        value: '0'
-      });
-      lightintAct.save();
-      const flowpumpAct = new Actuator({
-        label: 'Flow Pump',
-        type: 'flowpump',
-        value: '0'
-      });
-      flowpumpAct.save();
-      const watertempSen = new Sensor({
-        label: 'Water Temperature',
-        type: 'watertemp',
-        unit: '&deg;C'
-      });
-      watertempSen.save();
-      const node = new Node({
-        label: 'Development Node',
-        identity: 'DevTemp',
-        status: 0,
-        actuators: [
-          lightintAct.id,
-          flowpumpAct.id
-        ],
-        sensors: [
-          watertempSen.id
-        ]
-      });
-      node.save();
-      mqtt.send(topic, node.id);
-      console.log('New Node: ' + node.id);
+    switch (topic) {
+      case id:
+        const node = generateNewNode();
+        mqtt.send(topic, node.id);
+        console.log('New Node: ' + node.id);
+        break;
     }
   });
 
-  // TODO: Add sensor updates & make dynamic
+  /**
+   * Generate new node
+   */
+  function generateNewNode() {
+    // Actuators
+    const lightintAct = new Actuator({
+      label: 'Light Strip',
+      type: 'lightint',
+      value: '0'
+    });
+    lightintAct.save();
+    const flowpumpAct = new Actuator({
+      label: 'Flow Pump',
+      type: 'flowpump',
+      value: '0'
+    });
+    flowpumpAct.save();
+    const foodpumpAct = new Actuator({
+      label: 'Food Pump',
+      type: 'foodpump',
+      value: '0'
+    });
+    foodpumpAct.save();
+    // Sensors
+    const airtempSen = new Sensor({
+      label: 'Air Temperature',
+      type: 'airtemp',
+      unit: '&deg;C'
+    });
+    airtempSen.save();
+    const watertempSen = new Sensor({
+      label: 'Water Temperature',
+      type: 'watertemp',
+      unit: '&deg;C'
+    });
+    watertempSen.save();
+    const lightstrSen = new Sensor({
+      label: 'Light Strength',
+      type: 'lightstr',
+      unit: '&amp;'
+    });
+    lightstrSen.save();
+    const airhumiditySen = new Sensor({
+      label: 'Air Humidity',
+      type: 'airhumidity',
+      unit: '&amp;'
+    });
+    airhumiditySen.save();
+    const waterphSen = new Sensor({
+      label: 'Water Hardness',
+      type: 'waterph',
+      unit: '&amp;'
+    });
+    waterphSen.save();
+    // Node
+    const node = new Node({
+      label: 'Development Node',
+      identity: 'DevTemp',
+      status: 0,
+      actuators: [
+        lightintAct.id,
+        flowpumpAct.id,
+        foodpumpAct.id
+      ],
+      sensors: [
+        airtempSen.id,
+        watertempSen.id,
+        lightstrSen.id,
+        airhumiditySen.id,
+        waterphSen.id,
+      ]
+    });
+    node.save();
+    return node;
+  }
+
+  /**
+   * MQTT Server on Publish (Any)
+   */
   server.on('published', (packet, client) => {
-    // const topic = packet.topic.split('/');
-    // if (topic[0] != '$SYS')
-    //   if (topic[1] == 'light')
-    //     console.log('Light Strength: ' + packet.payload.toString());
-    //   else if (topic[1] == 'watertemp')
-    //     console.log('Air Temp: ' + packet.payload.toString());
-    //   else if (topic[1] == 'temp')
-    //     console.log('Water Temp: ' + packet.payload.toString());
-    //   else
-    //     console.log('Random');
+    const topic = serializeTopic(packet);
+    if(topic)
+      console.log(topic);
   });
+
+  /**
+   * Serialize and validate a received MQTT Packet
+   * @param {MQTTPacket} packet MQTT Packet
+   * @returns {Array(Topic.split)}
+   */
+  function serializeTopic(packet) {
+    topics = [
+      'lightstr',
+      'watertemp',
+      'airtemp',
+      'airhumidity',
+      'waterph',
+      'lightint',
+      'flowpump',
+      'foodpump'
+    ];
+    const topic = packet.topic.split('/');
+    if(topic[0] != '$SYS') {
+      if (topics.includes(topic[1])) {
+        return topic;
+      }
+    } else
+      return false;
+  }
 
   /*/// ROUTES ///*/
 
